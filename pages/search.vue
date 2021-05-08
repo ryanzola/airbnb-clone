748055434d86305c49b04356e69e32550f19e425
ryanzola <ryanzola@me.com>
2021-05-08 01:52:37 -0400

    Fleet-88 - Home Search Range
    [ ] ADDITIONS
    
    [ ] UPDATES
     - update search page to use selected date ranges.
    
    [ ] DELETIONS

--- pages/search.vue
+++ pages/search.vue
@@ -23,7 +23,7 @@ export default {
     }
   },
   async asyncData({ query, $dataApi }) {
-    const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
+    const data = await $dataApi.getHomesByLocation(query.lat, query.lng, query.start, query.end)
     return {
       homes: data.json.hits,
       label: query.label,
@@ -35,7 +35,7 @@ export default {
     this.updateMap();
   },
   async beforeRouteUpdate(to, from, next) {
-    const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng);
+    const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng, to.query.start, to.query.end)
     this.homes = data.json.hits;
     this.label = to.query.label;
     this.lat = to.query.lat;
